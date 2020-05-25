3961dd32a19367089c6a734f01d5d0e4d98b83ec
Samuel Rowe <samuelrowe1999@gmail.com>
2020-05-25 12:09:55 +0530

    Created kush.h

---
+++ include/kush.h
@@ -0,0 +1,25 @@
+// Monday, May 25 2020
+
+#ifndef KUSH_H
+#define KUSH_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/******************************************************************************
+ * String                                                                     *
+ ******************************************************************************/
+
+/**
+ * @author Samuel Rowe
+ * @since 0.1
+ */
+struct k_String_t {
+	uint8_t* m_value;
+	int32_t m_length;
+	uint32_t hashCode;
+};
+
+typedef k_String_t k_String_t;
+
+#endif /* KUSH_H */
